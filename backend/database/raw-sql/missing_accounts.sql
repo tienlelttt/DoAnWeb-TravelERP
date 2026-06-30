63224274b99413f79596780b4814dabd92be15d1
thuytranghihi <24521817@gm.uit.edu.vn>
2026-07-01 00:36:41 +0700

    thêm dữ liệu bị thiếu, reset mật khẩu

---
+++ backend/database/raw-sql/missing_accounts.sql
@@ -0,0 +1,69 @@
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_01', 'khach01', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Trần Minh Khoa', '079199000101', '1995-02-14', 'khach01@digitaltravel.vn', '0911000101', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_02', 'khach02', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Phạm Ngọc Linh', '079199000102', '1997-08-20', 'khach02@digitaltravel.vn', '0911000102', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_03', 'khach03', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Lê Thu Hà', '079199000103', '1992-11-03', 'khach03@digitaltravel.vn', '0911000103', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_04', 'khach04', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Nguyễn Bảo Châu', '079199000104', '1989-05-09', 'khach04@digitaltravel.vn', '0911000104', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_05', 'khach05', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Đỗ Quang Huy', '079199000105', '1986-12-25', 'khach05@digitaltravel.vn', '0911000105', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_06', 'khach06', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Bùi Anh Thư', '079199000106', '1999-04-18', 'khach06@digitaltravel.vn', '0911000106', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_07', 'khach07', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Hoàng Việt Anh', '079199000107', '1991-01-16', 'khach07@digitaltravel.vn', '0911000107', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_08', 'khach08', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Vũ Khánh Vy', '079199000108', '1994-09-27', 'khach08@digitaltravel.vn', '0911000108', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_09', 'khach09', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Đặng Gia Hân', '079199000109', '1988-03-30', 'khach09@digitaltravel.vn', '0911000109', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_10', 'khach10', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Mai Phương Nhi', '079199000110', '1996-06-12', 'khach10@digitaltravel.vn', '0911000110', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_11', 'khach11', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Cao Minh Trí', '079199000111', '1984-10-08', 'khach11@digitaltravel.vn', '0911000111', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_12', 'khach12', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Trịnh Mỹ Duyên', '079199000112', '1998-07-07', 'khach12@digitaltravel.vn', '0911000112', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_13', 'khach13', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Nguyễn Đức Long', '079199000113', '1985-09-19', 'khach13@digitaltravel.vn', '0911000113', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_14', 'khach14', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Lâm Tuệ Minh', '079199000114', '1990-02-28', 'khach14@digitaltravel.vn', '0911000114', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_15', 'khach15', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Phan Gia Bảo', '079199000115', '1993-12-02', 'khach15@digitaltravel.vn', '0911000115', 'KHACHHANG', 'HOAT_DONG');
+
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_16', 'khach16', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Nguyễn Thảo Nguyên', '079199000116', '1994-04-21', 'khach16@digitaltravel.vn', '0911000116', 'KHACHHANG', 'HOAT_DONG');
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_17', 'khach17', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Phạm Minh Anh', '079199000117', '1996-09-12', 'khach17@digitaltravel.vn', '0911000117', 'KHACHHANG', 'HOAT_DONG');
+INSERT INTO tai_khoans (ma_tai_khoan, ten_dang_nhap, mat_khau, ho_ten, cccd, ngay_sinh, email, so_dien_thoai, vai_tro, trang_thai)
+VALUES ('TK_KH_18', 'khach18', '$2y$10$2y$10$BBvBS1dGLV8lLRIF47sbfukbnxchs/ZbP6Gdb.JI2H5UZSeHOMmkKgoq7NsqwVDpICR7qmN41umhWrjjAWUA9k.Nc5vIYzYFxnOE.zMS6',
+        'Vũ Hoàng Yến', '079199000118', '1991-03-06', 'khach18@digitaltravel.vn', '0911000118', 'KHACHHANG', 'HOAT_DONG');
